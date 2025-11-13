1507df62ad034894731785a21b807f719429165e
eddie474 <eddie@aoa.edu>
2025-11-13 14:48:42 -0500

    Delete firebase_config.js

--- firebase_config.js
+++
@@ -1,8 +0,0 @@
-export const hardcodedFirebaseConfig = {
-  apiKey: "AIzaSyAK1umts0wkVAkCWqtQ_04DQcto78jbZaE",
-  authDomain: "flight-sim-leaderboard.firebaseapp.com",
-  projectId: "flight-sim-leaderboard",
-  storageBucket: "flight-sim-leaderboard.firebasestorage.app",
-  messagingSenderId: "1045910945297",
-  appId: "1:1045910945297:web:39417042ebdf9eda0bbbb0"
-};
